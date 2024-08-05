954bc40a2c7f2ba9ad0937f546c38fd99811205b
LukBartsch <lukasz.bartsch@gmail.com>
2024-08-05 14:59:56 +0200

    Add template for LineChart

--- frontend_react/re_prices_monitor/src/App.js
+++ frontend_react/re_prices_monitor/src/App.js
@@ -4,9 +4,42 @@ import './App.css';
 import React from 'react';
 //import api from './api.js';
 
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+
+import LineChart from "./components/LineChart";
+
 import { useEffect, useState } from "react";
 
+
+Chart.register(CategoryScale);
+
+
+
 function App() {
+
+
+  const [chartData, setChartData] = useState({
+    // ...chart data
+
+
+    // labels: Data.map((data) => data.year), 
+    // datasets: [
+    //   {
+    //     label: "Users Gained ",
+    //     data: Data.map((data) => data.userGain),
+    //     backgroundColor: [
+    //       "rgba(75,192,192,1)",
+    //       &quot;#ecf0f1",
+    //       "#50AF95",
+    //       "#f3ba2f",
+    //       "#2a71d0"
+    //     ],
+    //     borderColor: "black",
+    //     borderWidth: 2
+    //   }
+    // ]
+  });
 
 
   const [prices, setPrices] = useState([]);
