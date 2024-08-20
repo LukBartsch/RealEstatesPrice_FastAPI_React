27e193a811c43d1e26b2368146beb5d0281af5c1
LukBartsch <lukasz.bartsch@gmail.com>
2024-08-20 15:44:02 +0200

    Remove not necessary file

--- frontend_react/re_prices_monitor/src/App.js
+++ frontend_react/re_prices_monitor/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 
 import React from 'react';
-//import api from './api.js';
 
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
