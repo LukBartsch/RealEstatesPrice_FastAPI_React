8f1bf459531850868ebf86975dcfc06b6fec61ab
LukBartsch <lukasz.bartsch@gmail.com>
2025-03-21 20:19:39 +0100

    Refactor database path resolution and clean up debug print statements

--- backend_fast_api/database.py
+++ backend_fast_api/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-base_dir = Path(__file__).resolve().parent
+base_dir = Path(__file__).resolve().parent.parent
 env_file = os.path.join(base_dir, '.env')
 load_dotenv(env_file)
 
@@ -14,10 +14,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DB_FILE_PATH = os.path.join(base_dir, os.environ.get('DB_FILE_PATH')) # base_dir / os.environ.get('DB_FILE_PATH')
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') + os.environ.get('DB_FILE_PATH')
 
-print("DB_FILE_PATH:")
-print(SQLALCHEMY_DATABASE_URI)
+#print("DB_FILE_PATH:")
+#print(SQLALCHEMY_DATABASE_URI)
 
-#DB_URL = DB_URL = os.getenv("DB_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
 SessionLocal = sessionmaker(autocommit=False,autoflush=False, bind=engine)
 
